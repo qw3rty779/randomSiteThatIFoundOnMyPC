5bc151dcc7da08313f69ba1aa48041030dd4b89d
qw3rty779 <ghought@yandex.ru>
2026-05-19 23:07:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,44 @@ but1.addEventListener("click", (event) => {
 but2.addEventListener("click", (event) => {
     event.preventDefault();
 })
+
+var messageElement = document.querySelector('.cookie-notification');
+if (!Cookies.get('agreement')) {
+    showMessage();
+} else {
+    initCounter();
+}
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym')
+function addClass (o, c) {
+    var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g");
+    if (!o || re.test(o.className)) {
+        return;
+    }
+    o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "");
+    }
+function removeClass (o, c) {
+    var re = new RegExp('(^|\\s)' + c + '(\\s|$)', 'g');
+    if (!o) {
+        return;
+    }
+    o.className = o.className.replace(re, '$1').replace(/\s+/g, ' ').replace(/(^ | $)/g, '');
+}
+function hideMessage () {
+    addClass(messageElement, 'cookie-notification_hidden_yes');
+ }
+function showMessage () {
+    removeClass(messageElement, 'cookie-notification_hidden_yes');
+}
+function saveAnswer () {
+    hideMessage();
+    Cookies.set('agreement', '1');
+}
+function initCounter () {
+     ym(109308348, 'init', {});
+    saveAnswer();
+}
+document.querySelector('#yes').addEventListener('click', function () {
+    initCounter();
+});
