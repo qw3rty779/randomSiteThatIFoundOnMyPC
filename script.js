92fc8ac386350da037b348013b5b5e19aa4cebec
qw3rty779 <ghought@yandex.ru>
2026-05-19 20:46:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calc() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    if (num1 == 1) {
+        var pr = 3200000;
+    }
+    if (num1 == 2) {
+        var pr = 5100000;
+    }
+    if (num1 == 3) {
+        var pr = 7400000;
+    }
+    var res = pr * (0.0185 * Math.pow(1 + 0.0185, num2)) / (Math.pow(1 + 0.0185, num2) - 1);
+    document.getElementById("res").innerHTML = "Ежемесячный платёж: " + Math.round(res) + "₽";
+};
+
+elem.onclick = calc;
